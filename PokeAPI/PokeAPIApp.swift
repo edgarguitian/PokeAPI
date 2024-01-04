61d6d295e28dcf28d337a92094c17b48d6c38237
Edgar <edguitian@imagine800.es>
2024-01-04 19:39:51 +0100

    Initial Commit

---
+++ PokeAPI/PokeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAPIApp.swift
+//  PokeAPI
+//
+//  Created by Edgar Guitian Rey on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PokeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
