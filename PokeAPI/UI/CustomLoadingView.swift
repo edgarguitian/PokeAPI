55b20d60dbe16d03e1d0c6961381db40544b6084
Edgar <edguitian@imagine800.es>
2024-01-11 19:19:54 +0100

    pagination on pokemon list

---
+++ PokeAPI/UI/CustomLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomLoadingView.swift
+//  PokeAPI
+//
+//  Created by Edgar Guitian Rey on 11/1/24.
+//
+
+import SwiftUI
+
+struct CustomLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomLoadingView()
+}
